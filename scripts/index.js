65cff7909559ef7c1e9e2b9773648e5efa3a83ee
Eric Mauldin <emauldin84@gmail.com>
2019-03-12 15:52:52 -0400

    adds coffee.json file as reference

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,21 @@
-const API_URL = 'https://dc-coffeerun.herokuapp.com/api/coffeeOrders';
+const API_URL = '/coffee.json';
+let coffeeOrderArray = [];
+
+console.log(API_URL);
+
+// Draw all the email addresses associated with the coffee orders to the page in a list
+// fetch data from api url and return as object
+
+fetch(API_URL)
+.then (function(response) {
+    return response.json();
+})
+.then (function(coffeeOrderData) { 
+    coffeeOrderArray = coffeeOrderData;
+    console.log(coffeeOrderArray)
+})
+
+
+
+
 
